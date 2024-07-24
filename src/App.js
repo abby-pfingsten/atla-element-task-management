6772e7a76a4d2e78bcf07be22f08172013c9ac40
abby-pfingsten <abbypfin@yahoo.com>
2024-07-24 11:38:51 -0400

    added prettier

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+
+function App() {
+  return (
+    <div className="App">
+      hi
+      <BrowserRouter>
+        <Routes>
+          
+      </Routes>
+      
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
